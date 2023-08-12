dd47bc61758967eca3352bcc5f67813563658497
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-08-12 15:03:52 +0500

    Connecting backend for listing tasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import TasksCard from './components/TasksCard'
 
 function App() {
 
-  const tasks = [
-    {task_name:'Todo List',task_description:'I shell create first project of todo list'},
-    {task_name:'Chat App',task_description:'I shell create secont project of chat app'},
-    {task_name:'E-commerce app',task_description:'I shell create third project of ecommerce app'}
-  ]
+  // const tasks = [
+  //   {task_name:'Todo List',task_description:'I shell create first project of todo list'},
+  //   {task_name:'Chat App',task_description:'I shell create secont project of chat app'},
+  //   {task_name:'E-commerce app',task_description:'I shell create third project of ecommerce app'}
+  // ]
 
   return (
 
     <>
     <NavBar/>
-    <TasksCard task={tasks}/>
+    <TasksCard/>
     </>
 
 
--- src/components/TasksCard.tsx
+++ src/components/TasksCard.tsx
@@ -1,14 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 interface Task {
   task_name: string;
   task_description: string;
 }
 
-interface Prop {
-  task: Task[];
-}
 
-const TasksCard = ({ task }: Prop) => {
+const TasksCard = () => {
+  
+  const [task,setTask] = useState<Task[]>([])
+  const [error,setError] = useState('')
+
+
+  useEffect(()=>{
+    axios
+    .get<Task[]>('http://127.0.0.1:8000/task/')
+    .then(response=>setTask(response.data))
+    .catch(err=>setError(err))
+
+  })
   return (
+    <>
+    {error && <p className="text-danger">{error}</p>}
     <div
       className="card col-md-6"
       style={{ maxWidth: "100%", margin: "0px 10px 0px 10px" }}
@@ -17,13 +31,14 @@ const TasksCard = ({ task }: Prop) => {
         className="card-body custom-scrollbar"
         style={{ maxHeight: "400px", overflowY: "scroll" }}
       >
-        <ul className="list-group">
-          {task.map((task) => (
-            <li className="list-group-item">{task.task_name}</li>
+        <ul className="list-group" >
+          {task.map((e) => (
+            <li className="list-group-item" key={e.task_name}>{e.task_name}</li>
           ))}
         </ul>
       </div>
     </div>
+    </>
   );
 };
 

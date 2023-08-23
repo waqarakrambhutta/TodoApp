62a9654635c339c62780b65feb602edac899f21c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-08-23 19:30:30 +0500

    Adding comment

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -19,6 +19,7 @@ const AddTask = ({ onAddTask }: AddTaskFormProp) => {
 
 
   const onSubmit = async (data: any) => {
+    // use axios
     try {
       const response = await axios.post('http://127.0.0.1:8000/task/', data); 
       if (response.status === 201) {

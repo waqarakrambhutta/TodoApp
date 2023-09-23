39b52fab993be4e2786df85d5bfc0f1aab580bff
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-23 22:02:13 +0500

    Formally completed

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-interface Items {
+interface ItemProps {
   className?: string;
+  task: string;
 }
 
-const Item: React.FC<Items> = ({ className }: Items) => {
-  return <div className={`p-6 ${className}`}>Item</div>;
+const Item: React.FC<ItemProps> = ({ className, task }) => {
+  return (
+    <div
+      className={`px-6 rounded-lg shadow-md hover:shadow-lg py-2 border my-1 ${className}`}
+    >
+      {task}
+    </div>
+  );
 };
 
 export default Item;

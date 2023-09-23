aabc3d6c85a6f04ddb969aa5131b248151b66111
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-23 22:02:04 +0500

    Form done

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
+interface FormProps {
+  addTask: (newTask: string) => void;
 }
 
-export default Form
+const Form: React.FC<FormProps> = ({ addTask }) => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addTask(task);
+    setTask("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task}
+        placeholder="Enter a task..."
+        onChange={(e) => setTask(e.target.value)}
+        className="px-3 border-2 py-2 outline-none shadow-sm hover:shadow-md border-gray-300 rounded-md"
+      />
+      <button
+        type="submit"
+        className="ml-2 rounded-lg border-2  bg-white shadow-sm hover:shadow-md py-2 px-3"
+      >
+        ➕
+      </button>
+    </form>
+  );
+};
+
+export default Form;
